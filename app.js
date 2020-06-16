272e71fad0978e32e835740b8f01820b54803d30
unknown <im_tamazouzt@esi.dz>
2020-06-16 17:53:16 +0200

    Local Storage

--- app.js
+++ app.js
@@ -10,10 +10,61 @@ console.log(taskin.value);
 addEventListeners();
 function addEventListeners(e)
 {
+  document.addEventListener('DOMContentLoaded',gettasks)
     form.addEventListener('submit',addTask);
     liste.addEventListener('click',suppression);
      boutton.addEventListener('click',lavage);
      filtrer.addEventListener('keyup',recherche);
+}
+
+function gettasks()
+{
+  let taches ;
+  if(localStorage.getItem('taches')===null)
+  {
+    taches = [];
+  }
+  else
+  {
+    taches = JSON.parse(localStorage.getItem('taches'));
+  }
+  taches.forEach(function(t)
+  {
+    //const t = (JSON.stringify(taskk));
+    console.log(typeof(t) + typeof(t.task));
+    console.log(t + t['task'] + t.debut + t[0].task);
+    console.log(t.txt);
+    const li = document.createElement('li');
+  li.className='collection-item';
+  if(t.debut!='' && t.fin!='')
+  {
+  li.appendChild(document.createTextNode(t.debut+' --> ' +t.fin+' : ' +t.task ));
+  }
+  else
+  {
+    if(t.debut==''&&t.fin!='')
+    {
+      li.appendChild(document.createTextNode(t.fin+' : ' +t.task ));
+    }
+    else
+    {
+      if(t.debut!=''&&t.fin=='')
+      {
+        li.appendChild(document.createTextNode(t.debut + '  : ' +t.task ));
+      }
+      else
+      {
+        li.appendChild(document.createTextNode(t.task ));
+      }
+    }
+  }
+  const link = document.createElement('a');
+  link.className='delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+ liste.appendChild(li);
+  }
+  )
 }
 
 function addTask (e)
@@ -27,6 +78,7 @@ function addTask (e)
       
   const li = document.createElement('li');
   li.className='collection-item';
+ 
   if(temps.value!='' && fin.value!='')
   {
   li.appendChild(document.createTextNode(temps.value+' --> ' +fin.value+' : ' +taskin.value ));
@@ -54,12 +106,34 @@ function addTask (e)
   link.innerHTML = '<i class="fa fa-remove"></i>';
   li.appendChild(link);
  liste.appendChild(li);
+ storeLS(taskin.value,temps.value,fin.value);
   taskin.value='';
   temps.value='';
   fin.value='';
       
     }
   e.preventDefault();
+}
+
+function storeLS(Task,Begin,End)
+{
+  let taches =[];
+  if(localStorage.getItem('taches')==null)
+  {
+    taches = [];
+  }
+  else
+  {
+    taches = JSON.parse(localStorage.getItem('taches'));
+  }
+  let object = {task : Task , 
+  debut : Begin,
+fin : End};
+console.log(Task + Begin + End);
+console.log(taches +' '+JSON.stringify(object));
+  taches.push(JSON.stringify(object));
+    console.log(taches);
+    localStorage.setItem('taches',JSON.stringify(taches));
 }
 
 function suppression(e)
@@ -79,6 +153,9 @@ function lavage (e)
     boutton.previousElementSibling.removeChild(liste.firstChild);
 
   }
+  //let taches = [];
+  //localStorage.setItem('taches',null);
+  localStorage.clear();
   e.preventDefault();
 }
 function removing(e)
